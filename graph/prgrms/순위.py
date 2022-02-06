56f388fbf694abad0c92e7f5a08f8a934050c22a
JJ-HH <docessonhos@naver.com>
2022-02-06 19:25:57 +0900

    use set().update()

--- graph/prgrms/순위.py
+++ graph/prgrms/순위.py
@@ -9,9 +9,9 @@ def solution(n, results):
 
     for _self in range(*PLAYERS):
         for lower_ranker in wins[_self]:
-            losses[lower_ranker] |= losses[_self]
+            losses[lower_ranker].update(losses[_self])
         for higher_ranker in losses[_self]:
-            wins[higher_ranker] |= wins[_self]
+            wins[higher_ranker].update(wins[_self])
             
     ranked = [i for i in range(*PLAYERS) if len(wins[i] | losses[i]) == n-1]
     return len(ranked)
@@ -20,8 +20,4 @@ def solution(n, results):
 if __name__=="__main__":
     n = 5
     results = [[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]]
-    print(solution(n, results))
-
-    n = 5
-    results = [[3, 1], [2, 3], [4, 3], [2, 4], [4, 3], [4, 5]]
     print(solution(n, results))
